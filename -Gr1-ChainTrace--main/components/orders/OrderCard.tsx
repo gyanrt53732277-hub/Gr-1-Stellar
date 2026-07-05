524d1c4d2ba51e2b1803eed83395332eeeab8eed
Pra <gyanrt53732277@gmail.com>
2026-07-05 20:10:38 +0530

    Update OrderCard.tsx

--- -Gr1-ChainTrace--main/components/orders/OrderCard.tsx
+++ -Gr1-ChainTrace--main/components/orders/OrderCard.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from 'next/link'; 
 import { Badge } from '../ui/Badge';
 import { stellar } from '@/lib/stellar';
 import { FiArrowRight, FiUser, FiTruck, FiSearch } from 'react-icons/fi';
